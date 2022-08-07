d7f89deaf0fa5e9c8c0c8061e4c7fe74bbd1fff2
Esther Annorzie <estherannorzie@gmail.com>
2022-08-07 18:07:58 -0400

    Initial Commit

---
+++ Meal Planner iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meal Planner iOS
+//
+//  Created by Esther Annorzie on 8/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meal Planner iOS/Meal_Planner_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Planner_iOSApp.swift
+//  Meal Planner iOS
+//
+//  Created by Esther Annorzie on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Planner_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
